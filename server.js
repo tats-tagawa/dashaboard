f7779135ddc6661002d9f3ce0a4d7e6e12e1848f
Tats <10057207+tats-tagawa@users.noreply.github.com>
2023-03-24 22:24:37 -0700

    refactor: remove unused code

--- server.js
+++ server.js
@@ -15,16 +15,12 @@ import {
   getShapeCoordinates,
   updateOperatorDataTable,
 } from "./utils/sqlite.js";
-import realtime from "./routes/transit-realtime.js";
-import info from "./routes/transit-info.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use("/realtime", realtime);
-app.use("/info", info);
 
 app.listen(port, () => {
   console.log(`Dashaboard listening on port ${port}`);
